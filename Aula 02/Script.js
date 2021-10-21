22c7acea8ac5f919300de720b64f768cd4f99f01
Mariana Freitas <86315569+wxmariixw@users.noreply.github.com>
2021-10-21 11:54:32 -0700

    Add files via upload

---
+++ Aula 02/Script.js
@@ -0,0 +1,72 @@
+// tipos primitivos
+
+//boolean
+var vOuF = false;
+console.log (typeof(vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'Diana'
+console.log(typeof(nome));
+
+//como declarar
+var variavel = 'Diana';
+variavel = 'Martine';//Irá alterar
+console.log(variavel);
+
+let variavel2 = 'Diana';
+variavel2 = "Martine"; //Irá alterar
+
+const constante = 'Diana';
+constante = 'Martine'; //Vai voltar como erro
+console.log (constante);
+
+//Variável está sendo vista por todo código
+var escopoGlobal = 'Global';
+console.log(escopoGlobal);
+
+//Variável sento vista apenas dentro da função
+function escopolocal(){
+    let escopolocalinterno = 'local';
+    console.log(escopolocalinterno);
+}
+escopolocal();
+
+//Atribuição
+var atribuicao = 'Diana';
+
+//Comparação
+var comparacao = '0' == 0;
+
+//Comparação identica
+var comparcaoidentica = '0' === 0;
+
+//adicao
+var adicao = 1 + 1;
+console.log (adicao);
+
+//subtração
+var subtracao = 2 - 1;
+console.log (subtracao);
+
+//multiplicação
+var multiplicacao = 2 * 3;
+console.log (multiplicacao);
+
+//divisão real
+var divisaoreal = 6 / 2;
+console.log (divisaoreal);
+
+
+//divisão inteira
+var divisaointeira = 5 % 2
+console.log (divisaointeira);
+
+//potenciação
+var potenciacao = 2**10;
+console.log (potenciacao);
+
+
