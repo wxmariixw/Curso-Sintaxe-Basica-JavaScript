79d5f96514bd38ff0d77ddd31bed80a457187e0d
wxmariixw <marisftsabreu@gmail.com>
2021-10-28 13:09:28 -0300

    Finalização do curso de Iniciação a Javascript

---
+++ Aula 05/script.js
@@ -0,0 +1,10 @@
+function funcao () {
+    console.log ('Tudo certo jovem!');
+}
+funcao();
+
+function mensagem (primeiro, segundo) {
+    console.log (primeiro, segundo)
+}
+
+mensagem('tudo certo', 'jovem!');
---
+++ Aula 06/Script.js
@@ -0,0 +1,19 @@
+//Funções declarativas
+function funcao () {
+    console.log("Tudo certo jovem!")
+}
+funcao();
+
+//Expressões de funções (com ou sem nomeação)
+var funcao = function funcao() {
+    console.log(funcao)
+}
+funcao();
+
+//Arrow function
+var funcao = () => {
+    console.log(funcao)
+}
+funcao();
+
+//Funções aritméticas
